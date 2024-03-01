72ecb789690ed0e8849188bfa4cdbd015ff45f4a
wokaotianshi123 <56501836+wokaotianshi123@users.noreply.github.com>
2024-03-02 02:10:10 +0800

    Update install.sh

--- endip/install.sh
+++ endip/install.sh
@@ -34,7 +34,7 @@ cfwarpIP(){
 if [[ ! -f "warpendpoint" ]]; then
 echo "Download warp preferred program"
 if [[ -n $cpu ]]; then
-curl -L -o warpendpoint -# --retry 2 https://raw.githubusercontent.com/Ptechgithub/warp/main/endip/$cpu
+curl -L -o warpendpoint -# --retry 2 https://gitjs.wokaotianshi123.cloudns.org/https://raw.githubusercontent.com/Ptechgithub/warp/main/endip/$cpu
 fi
 fi
 }
